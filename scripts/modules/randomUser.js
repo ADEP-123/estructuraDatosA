f8f2ba6c1eb3076c693f1a8b8b658e174f6b5011
Elpanchez <santiagosanchez.ribero@gmail.com>
2024-05-19 14:47:42 -0500

    feat: :sparkles: Agregando más img de personas para la cola

--- scripts/modules/randomUser.js
+++ scripts/modules/randomUser.js
@@ -2,7 +2,7 @@ import Variables from "../variables.js";
 
 const variables = new Variables;
 function randomUser() {
-    const user = ["hombre1.png", "hombre2.png", "mujer1.png"]
+    const user = ["hombre1.png", "hombre2.png", "hombre3.png", "hombre4.png","mujer1.png", "mujer2.png", "abuelo1.png" ]
     let randomNum = Math.trunc(Math.random() * user.length);
     while (user[randomNum] == variables.lastUser) {
         randomNum = Math.trunc(Math.random() * (user.length - 0) + 0);
